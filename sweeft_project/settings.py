ba681d29b95126e7ed3627e58f3362d7985103a8
akadiving <akoivanishvili6@gmail.com>
2021-11-29 16:32:01 +0400

    v1: sweeft_api

--- sweeft_project/settings.py
+++ sweeft_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n0i08$-5&lo7)j+*#5=0_=xzx=rj)a9wboy(xm$x%6doy=_kf5'
+SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
